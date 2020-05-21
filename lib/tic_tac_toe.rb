a469103ccc631d25139decf10e228f67bd8afba1
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-21 04:27:58 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,67 @@
+#here are three test files that should be completed in order.
+#require_relative '../spec/01_tic_tac_toe_spec.rb'
+#tests for our helper methods within #TicTacToe. 
+#02_play_spec.rb tests the main #play method. 
+##03_cli_spec.rb tests the CLI
+
+class TicTacToe
+  
+  
+  
+  WIN_COMBINATIONS = 
+[
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+
+]
+
+  
+ def initialize(board = nil)
+    @board = board || Array.new(9,' ')
+ end
+  
+  
+def display_board
+  
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  
+end
+  
+def input_to_index(input)
+ index = input.to_i-1
+end 
+
+
+ def move(index, character="X")
+@board[index] = character
+end
+
+def position_taken?(index, character= "X" || "O")
+  if @board[index]== " "
+return  false
+ elsif @board[index]== nil || @board[index]= character
+  return true 
+end 
+  end 
+end
+
+def valid_move?(index)
+  if !position_taken?(index) && index.between?(0,8)
+    return true 
+end 
+end 
+
+  
+  def turn_count
+  @board.reject {|space| space == ' '}.length  
+end 
