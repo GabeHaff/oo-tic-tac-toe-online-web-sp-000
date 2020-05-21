38a8a79bb57109a0fd791277138035125ead10e0
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-21 05:01:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,9 +89,9 @@ def won?(board)
     win_index_2 = win_comb[1]
     win_index_3 = win_comb[2]
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_comb
